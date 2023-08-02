49ab17596bebd62cd4d51d5f7ab8bb8e8201e3e3
Dominique Makowski <dom.mak19@gmail.com>
2023-08-02 22:08:00 +0100

    TODO @AuzMoore

---
+++ experiment/dom_test/IllusionGameAltered.js
@@ -0,0 +1,63 @@
+// TODO: Rewrite these with the bespoke instructions
+
+// Mindfulness
+var mullerlyer_instructions_mindfulness =
+    "<p>In this part, two horizontal red lines will appear one above the other.</p>" +
+    "<p>Your task is to select which <b>line is longer</b> in length as fast as you can, without making errors.</p>" +
+    "<p>Don't get distracted by the surrounding black arrows at the end of the red lines!</p>" +
+    "<p>Press <b>the UP or the DOWN arrow</b> to indicate where is the longer <b>red line.</b></p>" +
+    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='300'></img>" +
+    "<p><img src='utils/answer/answer_updown_keyboard.PNG' height='150'></img></p>" +
+    "<p class='small'>In this example, the correct answer is the <b>UP arrow</b>.</p></div>" +
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
+
+var ebbinghaus_instructions_mindfulness =
+    "<p>In this part, two red circles will appear side by side on the screen.</p>" +
+    "<p>Your task is to select which <b>red circle is bigger</b> in size as fast as you can, without making errors.</p>" +
+    "<p>Don't get distracted by the surrounding black circles around the red circles!</p>" +
+    "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which is the bigger <b>red circle.</b></p>" +
+    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='300'></img>" +
+    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
+    "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
+
+var verticalhorizontal_instructions_mindfulness =
+    "<p>In this part, two red lines will appear side by side.</p>" +
+    "<p>Your task is to tell <b>which line is longer</b> in length, regardless of their orientation, as fast as you can, and without making errors.</p>" +
+    "<p>Don't get distracted by the orientation of the lines!</p>" +
+    "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which <b>line is the longer one.</b></p>" +
+    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='300'></img>" +
+    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
+    "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
+
+// Hypnosis
+var mullerlyer_instructions_mindfulness =
+    "<p>In this part, two horizontal red lines will appear one above the other.</p>" +
+    "<p>Your task is to select which <b>line is longer</b> in length as fast as you can, without making errors.</p>" +
+    "<p>Don't get distracted by the surrounding black arrows at the end of the red lines!</p>" +
+    "<p>Press <b>the UP or the DOWN arrow</b> to indicate where is the longer <b>red line.</b></p>" +
+    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='300'></img>" +
+    "<p><img src='utils/answer/answer_updown_keyboard.PNG' height='150'></img></p>" +
+    "<p class='small'>In this example, the correct answer is the <b>UP arrow</b>.</p></div>" +
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
+
+var ebbinghaus_instructions_mindfulness =
+    "<p>In this part, two red circles will appear side by side on the screen.</p>" +
+    "<p>Your task is to select which <b>red circle is bigger</b> in size as fast as you can, without making errors.</p>" +
+    "<p>Don't get distracted by the surrounding black circles around the red circles!</p>" +
+    "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which is the bigger <b>red circle.</b></p>" +
+    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='300'></img>" +
+    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
+    "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
+
+var verticalhorizontal_instructions_mindfulness =
+    "<p>In this part, two red lines will appear side by side.</p>" +
+    "<p>Your task is to tell <b>which line is longer</b> in length, regardless of their orientation, as fast as you can, and without making errors.</p>" +
+    "<p>Don't get distracted by the orientation of the lines!</p>" +
+    "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which <b>line is the longer one.</b></p>" +
+    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='300'></img>" +
+    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
+    "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
