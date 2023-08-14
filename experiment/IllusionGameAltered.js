30be3e2caf58bc82aa02e6844a17bb3b31819712
Dominique Makowski <dom.mak19@gmail.com>
2023-08-14 21:11:45 +0100

    fix size + instructions
    @AuzMoore I edited the altered instructions to make the more comparable can you double check

--- experiment/IllusionGameAltered.js
+++ experiment/IllusionGameAltered.js
@@ -2,63 +2,50 @@
 
 // Mindfulness
 var mullerlyer_mindfulness =
-    "<p>MINDFULNESS INSTRUCTIONS. In this phase, you will observe two horizontal red lines, one above the other.</p>" +
-    "<p>Direct your attention to discerning which <b>line is longer</b> in length. Do this at your own pace, without rushing, and strive for accuracy.</p>" +
-    "<p>Avoid allowing the surrounding black arrows at the ends of the red lines to distract you.</p>" +
-    "<p>Use the <b>UP or the DOWN arrow</b> to indicate the longer <b>red line.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='300'></img>" +
+    "<p>Now, you will have to select which <b>red line is longer</b> as fast as you can, without making errors.</p>" +
+    "<p>Remain in the state of mind you have been instructed, and use it to " +
+    "<b>focus your attention on the red lines</b>, and away from the black arrows.</p> " +
+    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='250'></img>" +
     "<p><img src='utils/answer/answer_updown_keyboard.PNG' height='150'></img></p>" +
-    "<p class='small'>In this example, the correct answer is the <b>UP arrow</b>.</p></div>" +
-    "<p>Are you prepared? <b>Press ENTER to begin</b></p>"
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
 var ebbinghaus_mindfulness =
-    "<p>MINDFULNESS INSTRUCTIONS. In this phase, two red circles will appear side by side on the screen.</p>" +
-    "<p>Your focus should be on identifying which <b>red circle is larger</b> in size. Take your time and avoid rushing to maintain accuracy.</p>" +
-    "<p>Dismiss any influence from the surrounding black circles around the red circles.</p>" +
-    "<p>Use the <b>LEFT or the RIGHT arrow</b> to indicate the larger <b>red circle.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='300'></img>" +
+    "<p>Now, you will have to select which <b>red circle is bigger</b> as fast as you can, without making errors.</p>" +
+    "<p>Remain in the state of mind you have been instructed, and use it to " +
+    "<b>focus your attention on the red circles</b>, and away from the black circles.</p> " +
+    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='250'></img>" +
     "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
-    "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
-    "<p>Are you ready? <b>Press ENTER to begin</b></p>"
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
 var verticalhorizontal_mindfulness =
-    "<p>MINDFULNESS INSTRUCTIONS. In this phase, you will observe two red lines placed side by side.</p>" +
-    "<p>Concentrate on determining which <b>line is longer</b> in length, irrespective of their orientations. Take your time, aim for accuracy, and avoid haste.</p>" +
-    "<p>Ignore any potential distraction from the orientation of the lines.</p>" +
-    "<p>Use the <b>LEFT or the RIGHT arrow</b> to indicate the longer <b>line.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='300'></img>" +
+    "<p>Now, you will have to select which <b>red line is longer</b> as fast as you can, without making errors.</p>" +
+    "<p>Remain in the state of mind you have been instructed, and use it to " +
+    "<b>focus your attention on the red lines</b>, and disregard their orientation.</p> " +
+    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='250'></img>" +
     "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
-    "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
-    "<p>Are you set? <b>Press ENTER to start</b></p>"
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
 // Hypnosis
 var mullerlyer_hypnosis =
-    "<p>HYPNOSIS INSTRUCTIONS. In this phase, two horizontal red lines will appear one above the other.</p>" +
-    "<p>You will find it natural to intuitively recognize which <b>line is longer</b> in length. Your responses will flow effortlessly, maintaining your focus and accuracy.</p>" +
-    "<p>Dismiss any influence from the surrounding black arrows at the ends of the red lines.</p>" +
-    "<p>Use the <b>UP or the DOWN arrow</b> to indicate the longer <b>red line.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='300'></img>" +
+    "<p>Now, you will have to select which <b>red line is longer</b> as fast as you can, without making errors.</p>" +
+    "<p>Remaining in the state of mind you have been instructed, you will be able to effortlessly see " +
+    "<b>only the red lines</b>, and you <b>won't even see or be affected by</b> the black arrows.</p> " +
+    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='250'></img>" +
     "<p><img src='utils/answer/answer_updown_keyboard.PNG' height='150'></img></p>" +
-    "<p class='small'>In this example, the correct answer is the <b>UP arrow</b>.</p></div>" +
-    "<p>Are you ready? <b>Press ENTER to begin</b></p>"
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
 var ebbinghaus_hypnosis =
-    "<p>HYPNOSIS INSTRUCTIONS. In this phase, two red circles will appear side by side on the screen.</p>" +
-    "<p>You will effortlessly determine which <b>red circle is larger</b> in size. Your responses will come naturally, and your accuracy will be inherent.</p>" +
-    "<p>Dismiss any influence from the surrounding black circles around the red circles.</p>" +
-    "<p>Use the <b>LEFT or the RIGHT arrow</b> to indicate the larger <b>red circle.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='300'></img>" +
+    "<p>Now, you will have to select which <b>red circle is bigger</b> as fast as you can, without making errors.</p>" +
+    "<p>Remaining in the state of mind you have been instructed, you will be able to effortlessly see " +
+    "<b>only the red circles</b>, and you <b>won't even see or be affected by</b> the black circles.</p> " +
+    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='250'></img>" +
     "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
-    "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
-    "<p>Are you prepared? <b>Press ENTER to begin</b></p>"
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
 var verticalhorizontal_hypnosis =
-    "<p>HYPNOSIS INSTRUCTIONS. In this phase, two red lines will appear side by side.</p>" +
-    "<p>You will inherently grasp <b>which line is longer</b> in length, irrespective of their orientations. Your discernment will be swift and accurate.</p>" +
-    "<p>Dismiss any influence from the orientation of the lines.</p>" +
-    "<p>Use the <b>LEFT or the RIGHT arrow</b> to indicate the longer <b>line.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='300'></img>" +
+    "<p>Now, you will have to select which <b>red line is longer</b> as fast as you can, without making errors.</p>" +
+    "<p>Remaining in the state of mind you have been instructed, you will be able to effortlessly see the red lines " +
+    "<b>without being influenced by their orientation</b>.</p> " +
+    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='250'></img>" +
     "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
-    "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
-    "<p>Are you set? <b>Press ENTER to begin</b></p>"
-
+    "<p>Are you ready? <b>Press ENTER to start</b></p>"
