46c4d951a58bf7a8faf624586695969d788724b0
DresLamora <ozzyruh2000@gmail.com>
2023-08-15 12:12:03 +0100

    sizeeeeeeee

--- experiment/IllusionGame.js
+++ experiment/IllusionGame.js
@@ -20,7 +20,7 @@ var mullerlyer_instructions =
     "<p>Don't get distracted by the surrounding black arrows at the end of the red lines!</p>" +
     "<p>Press <b>the UP or the DOWN arrow</b> to indicate where is the longer <b>red line.</b></p>" +
     "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='200'></img>" +
-    "<p><img src='utils/answer/answer_updown_keyboard.PNG' height='150'></img></p>" +
+    "<p><img src='utils/answer/answer_updown_keyboard.PNG' height='100'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>UP arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
@@ -30,7 +30,7 @@ var ebbinghaus_instructions =
     "<p>Don't get distracted by the surrounding black circles around the red circles!</p>" +
     "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which is the bigger <b>red circle.</b></p>" +
     "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='200'></img>" +
-    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
+    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='100'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
@@ -40,7 +40,7 @@ var verticalhorizontal_instructions =
     "<p>Don't get distracted by the orientation of the lines!</p>" +
     "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which <b>line is the longer one.</b></p>" +
     "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='200'></img>" +
-    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
+    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='100'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
