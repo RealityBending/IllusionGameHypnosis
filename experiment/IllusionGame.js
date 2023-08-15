f7864fbf21933cc7819a081deb4947bb8e6b0b1a
DresLamora <ozzyruh2000@gmail.com>
2023-08-15 12:09:58 +0100

    sizes

--- experiment/IllusionGame.js
+++ experiment/IllusionGame.js
@@ -19,7 +19,7 @@ var mullerlyer_instructions =
     "<p>Your task is to select which <b>line is longer</b> in length as fast as you can, without making errors.</p>" +
     "<p>Don't get distracted by the surrounding black arrows at the end of the red lines!</p>" +
     "<p>Press <b>the UP or the DOWN arrow</b> to indicate where is the longer <b>red line.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='250'></img>" +
+    "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='200'></img>" +
     "<p><img src='utils/answer/answer_updown_keyboard.PNG' height='150'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>UP arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
@@ -29,7 +29,7 @@ var ebbinghaus_instructions =
     "<p>Your task is to select which <b>red circle is bigger</b> in size as fast as you can, without making errors.</p>" +
     "<p>Don't get distracted by the surrounding black circles around the red circles!</p>" +
     "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which is the bigger <b>red circle.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='250'></img>" +
+    "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='200'></img>" +
     "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
@@ -39,7 +39,7 @@ var verticalhorizontal_instructions =
     "<p>Your task is to tell <b>which line is longer</b> in length, regardless of their orientation, as fast as you can, and without making errors.</p>" +
     "<p>Don't get distracted by the orientation of the lines!</p>" +
     "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which <b>line is the longer one.</b></p>" +
-    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='250'></img>" +
+    "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='200'></img>" +
     "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
